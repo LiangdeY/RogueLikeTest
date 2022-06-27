4460ab9e29624a989e7c4c8e150760f353851152
LiangDY <yldissaier@gmail.com>
2022-06-27 15:14:51 +0800

    Character basic move and turn
    - Added Character class
    - Added MoveForward / Turn Function

---
+++ Source/RogueLikeTest/RogueLikeTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RogueLikeTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROGUELIKETEST_API ARogueLikeTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
